f3ee8fd11b5856f587467a46e80b6ad27340fcd4
y-uti <y-uti@users.noreply.github.com>
2014-11-10 09:40:30 -0800

    Include folly/Portability.h to support gflags >= 2.1
    Summary:
    google-gflags changed it's namespace from ::google to ::gflags.  Both cases can be supported by including folly/Portability.h.
    
    Built with gflags-2.0 and gflags-2.1.1 (both on CentOS 7).
    Closes https://github.com/facebook/proxygen/pull/16
    Github Author: y-uti <y-uti@users.noreply.github.com>
    
    {sync, type="child", parent="github", parentrepo="facebook/proxygen", parentprnum="16", parentprfbid="732377286857200"}
    
    Test Plan: Imported from GitHub, without a `Test Plan:` line.
    
    Reviewed By: viswanath@fb.com
    
    Subscribers: doug, bmatheny
    
    FB internal diff: D1669879
    
    Signature: t1:1669879:1415650028:9d97027701a8732637afc2319ccf61ba8ee0a1ef

--- proxygen/lib/test/TestMain.cpp
+++ proxygen/lib/test/TestMain.cpp
@@ -8,13 +8,14 @@
  *
  */
 // Use this main function in gtest unit tests to enable glog
+#include <folly/Portability.h>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   LOG(INFO) << "Running tests from TestMain.cpp";
