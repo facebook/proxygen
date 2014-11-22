cd20a60380ee37e328936369372002a316af0cac
NagaChaitanya Vellanki <me@chaitanyavellanki.com>
2014-11-22 06:05:01 -0800

    make installation of glog, gflags and double-conversion consistent

--- proxygen/deps.sh
+++ proxygen/deps.sh
@@ -32,8 +32,8 @@ sudo apt-get install -yq \
 # deps.sh in fbthrift and folly builds anyways (no trap there)
 if ! sudo apt-get install libgoogle-glog-dev; 
 then
-	echo "fetching glog from svn (apt-get fails)"
 	if [ ! -e google-glog ]; then
+                echo "fetching glog from svn (apt-get failed)"
 		svn checkout https://google-glog.googlecode.com/svn/trunk/ google-glog
 		cd google-glog
 		./configure
@@ -43,24 +43,23 @@ then
 	fi
 fi
 
-if ! sudo apt-get install libgflags-dev; 
+if ! sudo apt-get install libgflags-dev;
 then
 	if [ ! -e google-gflags ]; then
-		echo "Fetching and building libgflags"
-		wget https://github.com/schuhschuh/gflags/archive/v2.0.tar.gz
-		mkdir google-gflags
-		tar xf v2.0.tar.gz -C google-gflags
-		cd google-gflags/gflags-2.0/
-		./configure
-		make
-		sudo make install
-		cd ../../
+                echo "Fetching gflags from svn (apt-get failed)"
+                svn checkout https://google-gflags.googlecode.com/svn/trunk/ google-gflags
+                cd google-gflags
+                ./configure
+                make
+                sudo make install
+                cd ..
 	fi
 fi
 
-if  ! sudo apt-get install libdouble-conversion-dev; 
+if  ! sudo apt-get install libdouble-conversion-dev;
 then
 	if [ ! -e double-conversion ]; then
+                echo "Fetching double-conversion from svn (apt-get failed)"
 		git clone https://code.google.com/p/double-conversion double-conversion
 		cd double-conversion
 		cmake . -DBUILD_SHARED_LIBS=ON
