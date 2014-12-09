648c2ed04ae7ec4e1104fd3ae52d58b3d44625aa
Sean Cannella <seanc@fb.com>
2014-12-08 16:25:48 -0800

    TransportInfo size mismatches
    Summary:
    The code that uses TransportInfo assumes these byte counts are
    compatible with size_t so make them so.
    
    Test Plan: compiled
    
    Reviewed By: davejwatson@fb.com
    
    Subscribers: fugalh, bmatheny, njormrod, folly-diffs@, benyluo, ranjeeth, fma, kmdent
    
    FB internal diff: D1725026
    
    Tasks: 5538843
    
    Signature: t1:1725026:1418061933:b4072ca926e792716776ddc00d79db9376510859

--- proxygen/lib/http/HTTPHeaderSize.h
+++ proxygen/lib/http/HTTPHeaderSize.h
@@ -17,22 +17,4 @@ namespace proxygen {
 
 typedef folly::HTTPHeaderSize HTTPHeaderSize;
 
-// /**
-//  * A structure that encapsulates byte counters related to the HTTP headers.
-//  */
-// struct HTTPHeaderSize {
-//   /**
-//    * The number of bytes used to represent the header after compression or
-//    * before decompression. If header compression is not supported, the value
-//    * is set to 0.
-//    */
-//   uint32_t compressed{0};
-
-//   /**
-//    * The number of bytes used to represent the serialized header before
-//    * compression or after decompression, in plain-text format.
-//    */
-//   uint32_t uncompressed{0};
-// };
-
 }
