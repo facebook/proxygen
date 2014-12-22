c207f70ef5cafded2e6de1a30bd026ee5b11e0e4
Alan Frindell <afrind@fb.com>
2014-12-22 11:00:54 -0800

    Move MockTimeoutManager from thrift to proxygen
    Summary:
    Only proxygen is using it, but maybe folly would be better?
    
    Facebook:
    ^
    
    Test Plan: Unit tests
    
    Reviewed By: davejwatson@fb.com
    
    Subscribers: noamler, doug, alandau, bmatheny, mshneer
    
    FB internal diff: D1746748
    
    Tasks: 5816910
    
    Signature: t1:1746748:1419027440:090c7820433ba72a21e081be8af09a1b8814aee6

---
+++ proxygen/lib/utils/test/MockTimeoutManager.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <folly/io/async/TimeoutManager.h>
+#include <chrono>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <glog/logging.h>
+
+namespace proxygen {
+
+class MockTimeoutManager : public folly::TimeoutManager {
+ public:
+
+  MOCK_METHOD2(
+      attachTimeoutManager,
+      void(folly::AsyncTimeout*, folly::TimeoutManager::InternalEnum));
+
+  MOCK_METHOD1(detachTimeoutManager, void(folly::AsyncTimeout*));
+
+  MOCK_METHOD2(
+      scheduleTimeout,
+      bool(folly::AsyncTimeout*, std::chrono::milliseconds));
+
+  MOCK_METHOD1(cancelTimeout, void(folly::AsyncTimeout*));
+
+  MOCK_METHOD0(bumpHandlingTime, bool());
+  MOCK_METHOD0(isInTimeoutManagerThread, bool());
+};
+
+} // proxygen
