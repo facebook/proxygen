212fdd75e43bc9501688953e7af5201fb832ce3d
David Callahan <dcallahan@fb.com>
2016-01-06 06:15:54 -0800

    parse args for -json flag
    Reviewed By: ttsugriy
    
    Differential Revision: D2806598
    
    fb-gh-sync-id: 8680a8ff84a2d352e31087e10f4422260ff8f510

--- proxygen/lib/utils/test/ResultBenchmark.cpp
+++ proxygen/lib/utils/test/ResultBenchmark.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -67,6 +67,7 @@ BENCHMARK(reference_medium_struct_or_byte, numIters) {
 }
 
 int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   folly::runBenchmarks();
   return 0;
 }
