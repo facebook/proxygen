47e062285e8655075dfaa7c719b6bcd777a5e97c
Chris Clark <chriscla@fb.com>
2016-01-08 16:23:12 -0800

    Fixing assignment/equality test error.
    Summary: Used = when needed ==
    
    Reviewed By: lbrandy
    
    Differential Revision: D2817318
    
    fb-gh-sync-id: f89442a37ee47bc060a2826708130af54c773918

--- proxygen/httpserver/filters/ZlibServerFilter.h
+++ proxygen/httpserver/filters/ZlibServerFilter.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -62,7 +62,7 @@ class ZlibServerFilter : public Filter {
 
   void sendChunkHeader(size_t len) noexcept override {
     // The headers should have always been sent since the message is chunked
-    DCHECK(header_ = true);
+    DCHECK_EQ(header_, true) << "Headers should have already been sent.";
 
     // If not compressing, pass downstream, otherwise "swallow" it
     // to send after compressing the body.
