184e8c43dc0ae0749e949be84cf49a11fa58cdde
Beny Luo <benyluo@fb.com>
2016-04-14 16:17:48 -0700

    Revert D3141866
    Summary:Revert D3141866 to avoid crash on race condition when session handler is
    deleted before TransactionAdaptor, we found this case at onNetwokSwitch on iOS.
    And I don't think there is easy way to handler the life
    cycle management.
    
    Differential Revision: D3181564
    
    fb-gh-sync-id: 61d927becca7ff234af335045c61d4bd64c898ef
    fbshipit-source-id: 61d927becca7ff234af335045c61d4bd64c898ef

--- proxygen/lib/http/session/HTTPTransaction.h
+++ proxygen/lib/http/session/HTTPTransaction.h
@@ -87,52 +87,6 @@ namespace proxygen {
  * different of HTTP-like protocols.
  */
 
-/** Info about Transaction running on this session */
-class TransactionInfo {
- public:
-  class TransactionInfoCallback {
-   public:
-    virtual ~TransactionInfoCallback() {}
-    virtual void onReportTransactionInfo(const TransactionInfo&) = 0;
-  };
-
-  TransactionInfo() {}
-
-  TransactionInfo(
-    std::chrono::milliseconds ttfb,
-    std::chrono::milliseconds ttlb,
-    uint64_t eHeader,
-    uint64_t inHeader,
-    uint64_t eBody,
-    uint64_t inBody,
-    bool completed):
-      timeToFirstByte(ttfb),
-      timeToLastByte(ttlb),
-      egressHeaderBytes(eHeader),
-      ingressHeaderBytes(inHeader),
-      egressBodyBytes(eBody),
-      ingressBodyBytes(inBody),
-      isCompleted(completed) {
-  }
-
-  /** Time to first byte */
-  std::chrono::milliseconds timeToFirstByte{0};
-  /** Time to last byte */
-  std::chrono::milliseconds timeToLastByte{0};
-
-  /** Number of bytes send in headers */
-  uint64_t egressHeaderBytes{0};
-  /** Number of bytes receive headers */
-  uint64_t ingressHeaderBytes{0};
-  /** Number of bytes send in body */
-  uint64_t egressBodyBytes{0};
-  /** Number of bytes receive in body */
-  uint64_t ingressBodyBytes{0};
-
-  /** Is the transaction was completed without error */
-  bool isCompleted{false};
-};
-
 class HTTPSessionStats;
 class HTTPTransaction;
 class HTTPTransactionHandler {
@@ -244,12 +198,6 @@ class HTTPTransactionHandler {
    * does not implement, better set max initiated to 0 in a settings frame?
    */
   virtual void onPushedTransaction(HTTPTransaction* txn) noexcept {}
-
-  /**
-   * set Transaction Info Callback to report transaction information on time
-   */
-  virtual void setTransactionInfoCallback(
-      TransactionInfo::TransactionInfoCallback*) noexcept {}
 
   virtual ~HTTPTransactionHandler() {}
 };
