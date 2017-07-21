1244a02cd6066e36815c4a3396aafc5853fcfdf7
Alan Frindell <afrind@fb.com>
2017-07-20 17:30:07 -0700

    Static webserver
    Summary: Read file named by path from a CPU thread pool
    
    Reviewed By: dddmello
    
    Differential Revision: D5261137
    
    fbshipit-source-id: 6608570e34f12cbdcce3dcce338682d21e35f114

---
+++ proxygen/httpserver/samples/static/StaticHandler.h
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (c) 2017, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include <folly/Memory.h>
+#include <folly/File.h>
+#include <proxygen/httpserver/RequestHandler.h>
+
+namespace proxygen {
+class ResponseHandler;
+}
+
+namespace StaticService {
+
+class StaticHandler : public proxygen::RequestHandler {
+ public:
+  void onRequest(std::unique_ptr<proxygen::HTTPMessage> headers)
+      noexcept override;
+
+  void onBody(std::unique_ptr<folly::IOBuf> body) noexcept override;
+
+  void onEOM() noexcept override;
+
+  void onUpgrade(proxygen::UpgradeProtocol proto) noexcept override;
+
+  void requestComplete() noexcept override;
+
+  void onError(proxygen::ProxygenError err) noexcept override;
+
+  void onEgressPaused() noexcept override;
+
+  void onEgressResumed() noexcept override;
+
+ private:
+  void readFile(folly::EventBase* evb);
+  bool checkForCompletion();
+
+  std::unique_ptr<folly::File> file_;
+  bool readFileScheduled_{false};
+  std::atomic<bool> paused_{false};
+  bool finished_{false};
+};
+
+}
---
+++ proxygen/httpserver/samples/static/StaticServer.cpp
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (c) 2017, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include <folly/portability/GFlags.h>
+#include <folly/Memory.h>
+#include <folly/init/Init.h>
+#include <folly/io/async/EventBaseManager.h>
+#include <proxygen/httpserver/HTTPServer.h>
+#include <proxygen/httpserver/RequestHandlerFactory.h>
+#include <wangle/concurrent/GlobalExecutor.h>
+#include <wangle/concurrent/CPUThreadPoolExecutor.h>
+#include <folly/portability/Unistd.h>
+
+#include "StaticHandler.h"
+
+using namespace StaticService;
+using namespace proxygen;
+
+using folly::EventBase;
+using folly::EventBaseManager;
+using folly::SocketAddress;
+
+using Protocol = HTTPServer::Protocol;
+
+DEFINE_int32(http_port, 11000, "Port to listen on with HTTP protocol");
+DEFINE_int32(h2_port, 11002, "Port to listen on with HTTP/2 protocol");
+DEFINE_string(ip, "localhost", "IP/Hostname to bind to");
+DEFINE_int32(threads, 0, "Number of threads to listen on. Numbers <= 0 "
+             "will use the number of cores on this machine.");
+
+namespace {
+
+class StaticHandlerFactory : public RequestHandlerFactory {
+ public:
+  void onServerStart(folly::EventBase* /*evb*/) noexcept override {}
+
+  void onServerStop() noexcept override {}
+
+  RequestHandler* onRequest(RequestHandler*, HTTPMessage*) noexcept override {
+    return new StaticHandler;
+  }
+};
+
+}
+
+int main(int argc, char* argv[]) {
+  folly::init(&argc, &argv, true);
+
+  std::vector<HTTPServer::IPConfig> IPs = {
+    {SocketAddress(FLAGS_ip, FLAGS_http_port, true), Protocol::HTTP},
+    {SocketAddress(FLAGS_ip, FLAGS_h2_port, true), Protocol::HTTP2},
+  };
+
+  if (FLAGS_threads <= 0) {
+    FLAGS_threads = sysconf(_SC_NPROCESSORS_ONLN);
+    CHECK_GT(FLAGS_threads, 0);
+  }
+
+  HTTPServerOptions options;
+  options.threads = static_cast<size_t>(FLAGS_threads);
+  options.idleTimeout = std::chrono::milliseconds(60000);
+  options.shutdownOn = {SIGINT, SIGTERM};
+  options.enableContentCompression = false;
+  options.handlerFactories = RequestHandlerChain()
+      .addThen<StaticHandlerFactory>()
+      .build();
+  options.h2cEnabled = true;
+
+  auto diskIOThreadPool = std::make_shared<wangle::CPUThreadPoolExecutor>(
+    FLAGS_threads,
+    std::make_shared<wangle::NamedThreadFactory>("StaticDiskIOThread"));
+  wangle::setCPUExecutor(diskIOThreadPool);
+
+  HTTPServer server(std::move(options));
+  server.bind(IPs);
+
+  // Start HTTPServer mainloop in a separate thread
+  std::thread t([&] () {
+    server.start();
+  });
+
+  t.join();
+  return 0;
+}
