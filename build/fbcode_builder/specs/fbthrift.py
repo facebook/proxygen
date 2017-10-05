20890bcc884c0fe9cdea4f47e4a1e28b035b001d
Fuat Geleri <geleri@fb.com>
2017-10-04 18:53:01 -0700

    Enable Yarpl for Thrift
    Summary: Uncomment the Yarpl reference in Thrift
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D5950572
    
    fbshipit-source-id: 83e864981ff17adde78ef7f6bddc2de6562b4657

--- build/fbcode_builder/specs/fbthrift.py
+++ build/fbcode_builder/specs/fbthrift.py
@@ -17,12 +17,22 @@ def fbcode_builder_spec(builder):
         'no1msd/mstch:git_hash',
         ShellQuoted('$(git describe --abbrev=0 --tags)')
     )
+    builder.add_option(
+        'rsocket/rsocket-cpp/yarpl/build:cmake_defines', {'BUILD_TESTS': 'OFF'}
+    )
     return {
         'depends_on': [folly, wangle, zstd],
         'steps': [
             # This isn't a separete spec, since only fbthrift uses mstch.
             builder.github_project_workdir('no1msd/mstch', 'build'),
             builder.cmake_install('no1msd/mstch'),
+            builder.github_project_workdir(
+                'rsocket/rsocket-cpp', 'yarpl/build'
+            ),
+            builder.step('configuration for yarpl', [
+                builder.cmake_configure('rsocket/rsocket-cpp/yarpl/build'),
+            ]),
+            builder.cmake_install('rsocket/rsocket-cpp/yarpl'),
             builder.fb_github_cmake_install('fbthrift/thrift'),
         ],
     }
