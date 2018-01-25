63775487ff09d33aff40ad96b82c05b46d24ff58
Adam Simpkins <simpkins@fb.com>
2018-01-24 16:16:11 -0800

    fbcode_builder: build folly using cmake
    Summary:
    Update the fbcode_builder spec for folly to build folly using its newer CMake
    build scripts, rather than autoconf.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D6793682
    
    fbshipit-source-id: b724cf8be4a1afe20bd96e6113395a4fd44c438a

--- build/fbcode_builder/specs/folly.py
+++ build/fbcode_builder/specs/folly.py
@@ -8,6 +8,6 @@ from __future__ import unicode_literals
 def fbcode_builder_spec(builder):
     return {
         'steps': [
-            builder.fb_github_autoconf_install('folly/folly'),
+            builder.fb_github_cmake_install('folly/folly'),
         ],
     }
