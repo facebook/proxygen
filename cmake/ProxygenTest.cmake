b4d68d707b649504659d1249c8227039647fa8b9
Alan Frindell <afrind@fb.com>
2019-05-16 10:05:31 -0700

    Adding missing copyright header
    Summary: as in title
    
    Reviewed By: yangchi
    
    Differential Revision: D15374093
    
    fbshipit-source-id: a9c7a231c20a8a504acc5640b698b62008eb2b00

--- cmake/ProxygenTest.cmake
+++ cmake/ProxygenTest.cmake
@@ -1,3 +1,9 @@
+#  Copyright (c) 2019, Facebook, Inc.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree.
+
 option(BUILD_TESTING  "Enable tests" OFF)
 include(CTest)
 if(BUILD_TESTING)
