afcc7440cdbc996302629a86783375ebf9f06199
Shilpa Gupta <shilgupta@fb.com>
2019-07-05 10:40:13 -0700

    open sourcing server list generator
    Summary: open sourcing basic server list generator, which reads server list from a text file.
    
    Reviewed By: bolekk
    
    Differential Revision: D15813789
    
    fbshipit-source-id: 78d148a00cb62acf8a1f989577710b1e7f5bdf7a

---
+++ proxygen/lib/pools/generators/FileServerListGenerator.h
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (c) 2015-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "proxygen/lib/pools/generators/ServerListGenerator.h"
+#include <proxygen/lib/utils/Exception.h>
+#include <sys/stat.h>
+
+namespace proxygen {
+
+/*
+ * A ServerListGenerator implementation that gets the server list from
+ * a file.
+ */
+class FileServerListGenerator : public ServerListGenerator {
+ public:
+  enum class FileType { PLAIN_TEXT, JSON };
+  // If FileType is specified as PLAIN_TEXT, we will read the file line by line.
+  // If FileType is specified as JSON, we will parse it as a json and take the
+  // entry that's named poolName.
+  explicit FileServerListGenerator(
+      const std::string& fileName,
+      const FileType fileType = FileType::PLAIN_TEXT,
+      const std::string& poolName = "");
+
+  void listServers(Callback* callback,
+                   std::chrono::milliseconds timeout) override;
+
+ protected:
+  struct Params {
+    explicit Params(const std::string& file,
+                    const FileType fileType,
+                    const std::string& pool)
+        : fileName(file), fileType(fileType), poolName(pool) {
+    }
+    std::string fileName;
+    FileType fileType;
+    std::string poolName;
+  };
+
+  class FileGenerator : public ServerListGenerator::Generator {
+   public:
+    FileGenerator(Params* params, Callback* callback)
+        : params_(params), callback_(callback) {
+    }
+    virtual ~FileGenerator() override {
+    }
+    virtual void readFile(std::string& filePath, std::string& content);
+    void run(std::chrono::milliseconds ms);
+    void cancelServerListRequest() override;
+
+   private:
+    Params* params_;
+    Callback* callback_;
+  };
+
+  Params params_;
+
+ private:
+  // Forbidden copy constructor and assignment operator
+  FileServerListGenerator(FileServerListGenerator const&) = delete;
+  FileServerListGenerator& operator=(FileServerListGenerator const&) = delete;
+};
+
+} // namespace proxygen
---
+++ proxygen/lib/pools/generators/ServerListGenerator.cpp
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (c) 2015-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#include "proxygen/lib/pools/generators/ServerListGenerator.h"
+
+#include <folly/Conv.h>
+#include <folly/io/async/EventBase.h>
+
+using folly::EventBase;
+using proxygen::ServerListGenerator;
+using std::vector;
+using std::chrono::milliseconds;
+
+namespace {
+
+class ServerListCallback : public ServerListGenerator::Callback {
+ public:
+  enum StatusEnum {
+    NOT_FINISHED,
+    SUCCESS,
+    ERROR,
+    CANCELLED,
+  };
+
+  explicit ServerListCallback() : status(NOT_FINISHED) {
+  }
+
+  void onServerListAvailable(
+      vector<ServerListGenerator::ServerConfig>&& results) noexcept override {
+    servers.swap(results);
+    status = SUCCESS;
+  }
+  void onServerListError(std::exception_ptr error) noexcept override {
+    errorPtr = error;
+    status = ERROR;
+  }
+  virtual void serverListRequestCancelled() {
+    status = CANCELLED;
+  }
+
+  StatusEnum status;
+  vector<ServerListGenerator::ServerConfig> servers;
+  std::exception_ptr errorPtr;
+};
+
+} // unnamed namespace
+
+namespace proxygen {
+
+void ServerListGenerator::attachEventBase(EventBase* base) {
+  CHECK(!eventBase_);
+  CHECK(base->isInEventBaseThread());
+
+  eventBase_ = base;
+}
+
+void ServerListGenerator::detachEventBase() {
+  CHECK(!eventBase_ || eventBase_->isInEventBaseThread());
+
+  eventBase_ = nullptr;
+}
+
+void ServerListGenerator::listServersBlocking(vector<ServerConfig>* results,
+                                              milliseconds timeout) {
+  // Run a EventBase to drive the asynchronous listServers() call until it
+  // finishes.
+  EventBase eventBase;
+  ServerListCallback callback;
+  attachEventBase(&eventBase);
+  listServers(&callback, timeout);
+  eventBase.loop();
+  detachEventBase();
+
+  if (callback.status != ServerListCallback::SUCCESS) {
+    if (!callback.errorPtr) {
+      LOG(FATAL) << "ServerListGenerator finished without invoking callback";
+    }
+    std::rethrow_exception(callback.errorPtr);
+  }
+
+  results->swap(callback.servers);
+}
+
+} // namespace proxygen
