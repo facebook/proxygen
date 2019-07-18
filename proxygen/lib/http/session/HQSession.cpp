050b9375887cca4d9d54d1bfa788ff12871d7401
Matt Joras <mjoras@fb.com>
2019-07-18 12:03:12 -0700

    Increase HQ draft versions
    Summary: We are currently on draft-22.
    
    Reviewed By: lnicco
    
    Differential Revision: D16358915
    
    fbshipit-source-id: 8e5dca8125d234cdb31f8f98a06152c8e8a9c8aa

--- proxygen/lib/http/session/HQSession.cpp
+++ proxygen/lib/http/session/HQSession.cpp
@@ -64,8 +64,8 @@ using namespace proxygen::hq;
 namespace proxygen {
 
 const std::string kH3FBCurrentDraft("h3-fb-05");
-const std::string kH3CurrentDraft("h3-20");
-const std::string kHQCurrentDraft("hq-20");
+const std::string kH3CurrentDraft("h3-22");
+const std::string kHQCurrentDraft("hq-22");
 
 const http2::PriorityUpdate hqDefaultPriority{kSessionStreamId, false, 15};
 
