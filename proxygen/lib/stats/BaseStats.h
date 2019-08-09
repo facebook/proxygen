257019f6a78d0986620a8bb19d2f05db03a78127
Bolek Kulbabinski <bolek@fb.com>
2019-08-09 09:06:46 -0700

    Open source BaseStats
    Summary: Move BaseStats.h into top-level lib directory.
    
    Reviewed By: dddmello
    
    Differential Revision: D16706425
    
    fbshipit-source-id: 7ec8f45ba8f0b70cd92e57ee7f07fb565cc13e2a

---
+++ proxygen/lib/stats/BaseStats.h
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2004-present, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "fb303/ThreadCachedServiceData.h"
+
+namespace proxygen {
+
+/*
+ * Counter definitions for use in child classes.  Updating all
+ * children thus becomes as simple as updating these definitions.
+ * It is thus intended and recommended for all callers to refer to
+ * BaseStats::<counter> when wishing to use counters.
+ */
+class BaseStats {
+ private:
+  // Private constructor so its clear nothing else should implement this class
+  BaseStats() = default;
+
+ public:
+  // TODO: given the simple nature of TLCounter and that it is explicitly
+  // thread safe via the use of atomics, we may only want single local
+  // instance instead of wrapped (per thread) instances.
+  using TLCounter = facebook::fb303::CounterWrapper;
+  using TLTimeseries = facebook::fb303::TimeseriesPolymorphicWrapper;
+  using TLTimeseriesQuarterMinuteOnly =
+      facebook::fb303::QuarterMinuteOnlyTimeseriesWrapper;
+  using TLTimeseriesMinute = facebook::fb303::MinuteOnlyTimeseriesWrapper;
+  using TLTimeseriesMinuteAndAllTime = facebook::fb303::MinuteTimeseriesWrapper;
+  using TLHistogram = facebook::fb303::HistogramWrapper;
+};
+
+} // namespace proxygen
