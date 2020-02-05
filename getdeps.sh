01e1ac75ae22f73801d62580c1dc5a686b8e202f
Luca Niccolini <lniccolini@fb.com>
2020-02-05 08:52:01 -0800

    getdeps build script
    Summary:
    Adds a new build script (based on getdeps).
    This is the same build that is running in Github Actions.
    
    Reviewed By: dddmello
    
    Differential Revision: D19386807
    
    fbshipit-source-id: 7a1c0730528e93bb390b6fc584a4fb6b00b5f758

---
+++ getdeps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -xeo pipefail
+
+TOOLCHAIN_DIR=/opt/rh/devtoolset-8/root/usr/bin
+if [[ -d "$TOOLCHAIN_DIR" ]]; then
+    PATH="$TOOLCHAIN_DIR:$PATH"
+fi
+
+PROXYGEN_DIR=$(dirname "$0")
+GETDEPS_PATHS=(
+    "$PROXYGEN_DIR/build/fbcode_builder/getdeps.py"
+    "$PROXYGEN_DIR/../../opensource/fbcode_builder/getdeps.py"
+)
+
+ROOT_DIR=$(pwd)
+STAGE=${ROOT_DIR}/_build/
+mkdir -p "$STAGE"
+
+for getdeps in "${GETDEPS_PATHS[@]}"; do
+    if [[ -x "$getdeps" ]]; then
+        "$getdeps" build proxygen --current-project proxygen "$@"
+        exit 0
+    fi
+done
+
+echo "Could not find getdeps.py!?" >&2
+exit 1
