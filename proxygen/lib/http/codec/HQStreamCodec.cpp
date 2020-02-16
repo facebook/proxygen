9a4a27e6b85b59efd665b1001a41151cff83eb7f
Yang Chi <yangchi@fb.com>
2020-02-15 21:05:19 -0800

    Declare HQStreamCodec ThreadLocals as auto local on mobile
    Summary: The speedup from declaring them as static variable isn't necessary on mobile. Instead it introduces a crash where the system kills the app and cleans up static objects but proxygen eventbase keeps running and using these variables.
    
    Reviewed By: mjoras
    
    Differential Revision: D19910586
    
    fbshipit-source-id: 9c9d804f717740c6245740a79ed616b4e4ac6c7b

--- proxygen/lib/http/codec/HQStreamCodec.cpp
+++ proxygen/lib/http/codec/HQStreamCodec.cpp
@@ -15,6 +15,18 @@
 #include <proxygen/lib/http/codec/HQUtils.h>
 #include <proxygen/lib/http/codec/compress/QPACKCodec.h>
 
+/**
+ * On some mobile platforms we don't always get a chance to stop proxygen
+ * eventbase. That leads to static object being cleaned up by system while
+ * proxygen tries to access them, which is bad. The speedup from making
+ * some variable static isn't necessary on mobile clients anyway. 
+ */
+#ifdef FOLLY_MOBILE
+#define HQ_STATIC
+#else
+#define HQ_STATIC static
+#endif
+
 namespace proxygen { namespace hq {
 
 using namespace folly;
@@ -508,8 +520,8 @@ void HQStreamCodec::generateHeaderImpl(folly::IOBufQueue& writeBuf,
                                        const HTTPMessage& msg,
                                        folly::Optional<StreamID> pushId,
                                        HTTPHeaderSize* size) {
-  static folly::ThreadLocal<std::vector<std::string>> tempsTL;
-  static folly::ThreadLocal<std::vector<compress::Header>> allHeadersTL;
+  HQ_STATIC folly::ThreadLocal<std::vector<std::string>> tempsTL;
+  HQ_STATIC folly::ThreadLocal<std::vector<compress::Header>> allHeadersTL;
   auto& temps = *tempsTL.get();
   auto& allHeaders = *allHeadersTL.get();
   temps.clear();
