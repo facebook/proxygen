1a8c6ed94bc6e496d96240f3edbf5d6479ecfc2c
Ameya Shendarkar <ameyas@fb.com>
2020-03-05 09:50:40 -0800

    Remove expired PSKs from quic cache
    Summary: This change removes expired PSKs from quic cache, as they shouldn't be probably used anyway.
    
    Reviewed By: knekritz
    
    Differential Revision: D20123049
    
    fbshipit-source-id: c3cf0be819d097338099061035c6bc51212c8dfd

--- proxygen/lib/http/SynchronizedLruQuicPskCache.cpp
+++ proxygen/lib/http/SynchronizedLruQuicPskCache.cpp
@@ -19,6 +19,15 @@ folly::Optional<quic::QuicCachedPsk> SynchronizedLruQuicPskCache::getPsk(
   auto cacheMap = cache_.wlock();
   auto result = cacheMap->find(identity);
   if (result != cacheMap->end()) {
+    if (std::chrono::system_clock::now() >
+        result->second.cachedPsk.ticketExpirationTime) {
+      VLOG(1) << "PSK expired: " << identity << ", id: "
+              << (result->second.cachedPsk.serverCert
+                      ? result->second.cachedPsk.serverCert->getIdentity()
+                      : "none");
+      cacheMap->erase(result);
+      return folly::none;
+      }
     return result->second;
   } else {
     return folly::none;
