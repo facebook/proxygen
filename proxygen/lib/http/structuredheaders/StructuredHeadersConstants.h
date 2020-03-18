f9d37200740d2dcc667fc05d478b3c448e3527f5
Federico Larumbe <fede@fb.com>
2020-03-18 09:32:17 -0700

    Add constructor for StructuredHeaderItem struct
    Summary: Add constructor for StructuredHeaderItem struct.
    
    Reviewed By: mengz0
    
    Differential Revision: D20503707
    
    fbshipit-source-id: d2433a8c3d2d88107ba3a6f567a19e5125633968

--- proxygen/lib/http/structuredheaders/StructuredHeadersConstants.h
+++ proxygen/lib/http/structuredheaders/StructuredHeadersConstants.h
@@ -8,41 +8,36 @@
 
 #pragma once
 
-#include <unordered_map>
-#include <map>
-#include <vector>
 #include <boost/variant.hpp>
+#include <map>
+#include <unordered_map>
 #include <vector>
 
-namespace proxygen {
-
-namespace StructuredHeaders {
+namespace proxygen { namespace StructuredHeaders {
 
 static const int kMaxValidIntegerLength = 19;
 static const int kMaxValidFloatLength = 16;
 
 /* tagged union for an item in a structured header */
 class StructuredHeaderItem {
-public:
-  enum class Type {
-    NONE,
-    STRING,
-    BINARYCONTENT,
-    IDENTIFIER,
-    DOUBLE,
-    INT64
-  };
+ public:
+  enum class Type { NONE, STRING, BINARYCONTENT, IDENTIFIER, DOUBLE, INT64 };
+  StructuredHeaderItem() = default;
+  StructuredHeaderItem(
+      Type tagIn, boost::variant<int64_t, double, const std::string&> valueIn)
+      : tag(tagIn), value(valueIn) {
+  }
 
-  template<typename T>
+  template <typename T>
   bool operator!=(const T& other) const {
     return !operator==(other);
   }
 
-  template<typename T>
+  template <typename T>
   bool operator==(const T& other) const {
     try {
       return boost::get<T>(value) == other;
-    } catch (boost::bad_get& ) {
+    } catch (boost::bad_get&) {
       return false;
     }
   }
@@ -57,7 +52,7 @@ struct ParameterisedIdentifier {
 };
 
 using ParameterisedList =
-  std::vector<StructuredHeaders::ParameterisedIdentifier>;
+    std::vector<StructuredHeaders::ParameterisedIdentifier>;
 
 using Dictionary = std::unordered_map<std::string, StructuredHeaderItem>;
 
@@ -80,26 +75,21 @@ enum class EncodeError : uint8_t {
   ENCODING_NULL_ITEM = 5
 };
 
-static const std::map<DecodeError, std::string>
-  decodeErrorDescription {
+static const std::map<DecodeError, std::string> decodeErrorDescription{
     {DecodeError::OK, "No error"},
     {DecodeError::VALUE_TOO_LONG, "Numeric value is too long"},
     {DecodeError::INVALID_CHARACTER, "Invalid character"},
     {DecodeError::UNDECODEABLE_BINARY_CONTENT, "Undecodable binary content"},
     {DecodeError::UNEXPECTED_END_OF_BUFFER, "Unexpected end of buffer"},
     {DecodeError::UNPARSEABLE_NUMERIC_TYPE, "Unparseable numeric type"},
-    {DecodeError::DUPLICATE_KEY, "Duplicate key found"}
-};
+    {DecodeError::DUPLICATE_KEY, "Duplicate key found"}};
 
-static const std::map<EncodeError, std::string>
-  encodeErrorDescription {
+static const std::map<EncodeError, std::string> encodeErrorDescription{
     {EncodeError::OK, "No error"},
     {EncodeError::EMPTY_DATA_STRUCTURE, "Empty data structure"},
     {EncodeError::BAD_IDENTIFIER, "Bad identifier"},
     {EncodeError::BAD_STRING, "Bad string"},
     {EncodeError::ITEM_TYPE_MISMATCH, "Item type mismatch"},
-    {EncodeError::ENCODING_NULL_ITEM, "Tried to encode null item"}
-};
+    {EncodeError::ENCODING_NULL_ITEM, "Tried to encode null item"}};
 
-}
-}
+}} // namespace proxygen::StructuredHeaders
