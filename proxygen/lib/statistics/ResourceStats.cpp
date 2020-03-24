2d030d8d14fc35a23e36e612d03257515df5e304
Darrin D'Mello <ddmello@fb.com>
2020-03-24 12:50:18 -0700

    Abstract out PeriodicStats to provide subclasses more customization
    Summary: By pulling out PeriodicStats, other implementations can now leverage this base class to return other data classes beyond just ResourceData.
    
    Reviewed By: jalopezsilva
    
    Differential Revision: D20297552
    
    fbshipit-source-id: 505aa340aecdcd2c7439a542d4d93099a60ed715

--- proxygen/lib/statistics/ResourceStats.cpp
+++ proxygen/lib/statistics/ResourceStats.cpp
@@ -8,81 +8,16 @@
 
 #include "proxygen/lib/statistics/ResourceStats.h"
 
-#include <folly/synchronization/Rcu.h>
-#include <glog/logging.h>
-
 namespace proxygen {
 
 ResourceStats::ResourceStats(std::unique_ptr<Resources> resources)
-    : resources_(std::move(resources)),
-      data_(new ResourceData(resources_->getCurrentData())) {
+    : PeriodicStats<ResourceData>(
+          new ResourceData(resources->getCurrentData())),
+      resources_(std::move(resources)) {
 }
 
-ResourceStats::~ResourceStats() {
-  stopRefresh();
-  modifyData(nullptr);
-}
-
-void ResourceStats::refreshWithPeriod(std::chrono::milliseconds periodMs) {
-  CHECK_GE(periodMs.count(), 0);
-  std::lock_guard<std::mutex> guard(schedulerMutex_);
-  refreshPeriodMs_ = periodMs;
-  if (!scheduler_) {
-    scheduler_.reset(new folly::FunctionScheduler());
-    scheduler_->setThreadName("sys_stats");
-    // Steady here implies that scheduling will be fixed as opposed to
-    // offsetting from the current time which is desired to ensure minimal
-    // use of synchronization for getCurrentLoadData()
-    scheduler_->setSteady(true);
-
-    std::function<void()> updateFunc(
-        std::bind(&ResourceStats::updateCachedData, this));
-    std::function<std::chrono::milliseconds()> intervalFunc(
-        std::bind(&ResourceStats::getRefreshIntervalMs, this));
-
-    scheduler_->addFunctionGenericDistribution(updateFunc,
-                                               intervalFunc,
-                                               "sys_stats",
-                                               "sys_stats_interval",
-                                               std::chrono::milliseconds(0));
-
-    scheduler_->start();
-  }
-}
-
-void ResourceStats::stopRefresh() {
-  std::lock_guard<std::mutex> guard(schedulerMutex_);
-  scheduler_.reset();
-}
-
-const ResourceData& ResourceStats::getCurrentLoadData() const {
-  {
-    folly::rcu_reader guard;
-    auto* loadedData = data_.load();
-    if (loadedData->getLastUpdateTime() != tlData_->getLastUpdateTime()) {
-      // Should be fine using the default assignment operator the compiler
-      // gave us I think...this will stop being true if loadedData starts
-      // storing pointers.
-      *tlData_ = *loadedData;
-    }
-  }
-  return *tlData_;
-}
-
-const ResourceData& ResourceStats::getPreviousLoadData() const {
-  return *tlData_;
-}
-
-void ResourceStats::updateCachedData() {
-  modifyData(new ResourceData(resources_->getCurrentData()));
-}
-
-void ResourceStats::modifyData(ResourceData* newData) {
-  auto* oldData = data_.load();
-  // Default copy constructor should be fine here much as above...this will
-  // stop being true if data starts storing pointers.
-  data_.store(newData);
-  folly::rcu_retire(oldData);
+ResourceData* ResourceStats::getNewData() const {
+  return new ResourceData(resources_->getCurrentData());
 }
 
 } // namespace proxygen
--- proxygen/lib/statistics/ResourceStats.h
+++ proxygen/lib/statistics/ResourceStats.h
@@ -8,11 +8,8 @@
 
 #pragma once
 
+#include "proxygen/lib/statistics/PeriodicStats.h"
 #include "proxygen/lib/statistics/ResourceData.h"
-#include <atomic>
-#include <chrono>
-#include <folly/ThreadLocal.h>
-#include <folly/experimental/FunctionScheduler.h>
 
 namespace proxygen {
 
@@ -20,114 +17,29 @@ namespace proxygen {
  * ResourceStats:
  *
  * A class designed to abstract away the internals of retrieving
- * various resource utilization metrics.  The main goal is to provide
- * a thread-safe mechanism for retrieving desired information (i.e. multiple
- * readers, single writer).  In this way readers view a consistent
- * representation of the underlying data, as opposed to a model where each
- * reader fetches the data separately, potentially at different times, and thus
- * potentially coming to different conclusions (not to mention wasting
- * resources performing the same operations).  Also consider the future
- * extensibility potential especially in the context of second order stats and
- * counter aggregation.
- *
- * All public methods are thread-safe (incl cache coherence).
+ * various resource utilization metrics, built using PeriodicStats.
+ * See PeriodicStats class documentation for specifics.
  */
-class ResourceStats {
+class ResourceStats : public PeriodicStats<ResourceData> {
  public:
   /**
-   * Default constructor that will initialize data_ as appropriate.
    * Note: as CPU pct utilization requires intervals and at init time there
    * is only a single data point, pct utilization is initially seeded from
-   * proc loadavg.  Underlying thread that updates locally cached Data is not
-   * started yet.
+   * proc loadavg.
    */
   explicit ResourceStats(std::unique_ptr<Resources> resources);
 
-  virtual ~ResourceStats();
-
-  // Returns the current refresh interval in ms of the underlying data.
-  std::chrono::milliseconds getRefreshIntervalMs() {
-    return refreshPeriodMs_;
-  }
-
-  /**
-   * Refreshes the underlying resource utilization data using the period
-   * specified by employing the underlying function scheduler which will
-   * subsequently create a thread.  If already refreshing, the current
-   * period will instead be updated and on next scheduling the updated period
-   * applied.
-   */
-  void refreshWithPeriod(std::chrono::milliseconds periodMs);
-
-  /**
-   * Stops refreshing the underlying resource utilization data.  It is
-   * possible that an update is occuring as this method is run in which case
-   * the guarantee becomes that no subsequent refresh will be scheduled.
-   */
-  void stopRefresh();
-
-  /**
-   * Readers utilize this method to get their thread local representation
-   * of the current resource utilization metrics.  The way in which this works
-   * is that while there is a thread local representation of the metrics,
-   * there is also a global representation that is guarded.  Asynchronously,
-   * the refresh thread will update the global representation in a thread safe
-   * manner.  Then, as readers wish to consume this data, they are instead
-   * presented with a thread local representation that is updated if:
-   *  A) their representation is uninitialized
-   *  B) their representation is old
-   * Note for case (B) there is no guarantee that a newer version is
-   * actually available (as is the way with threading).  Such situations
-   * should only occur if the driving thread is backlogged or if one or more
-   * stray readers are holding onto the mutex for some reason but regardless
-   * performance is NOT severely degraded; a read lock acquire and a
-   * fundamental type check is all that is performed.
-   *
-   * Method is virtual for testing reasons.
-   */
-  virtual const ResourceData& getCurrentLoadData() const;
-  // Same as above except no local update will be performed, even if newer
-  // data is available.
-  virtual const ResourceData& getPreviousLoadData() const;
-
  protected:
-  void updateCachedData();
+  /**
+   * Override getNewData so that we can return an instance of ResourceData.
+   */
+  ResourceData* getNewData() const;
 
   /**
    * Abstraction that enables callers to provide their own implementations
    * of the entity that actually queries various metrics.
    */
   std::unique_ptr<Resources> resources_;
-
-  /**
-   * data_ represents the source of truth for the various resource fields.
-   * tlData_ is updated on a 'as-used' basis from data via RCU
-   * synchronization.
-   */
-  std::atomic<ResourceData*> data_;
-  folly::ThreadLocal<ResourceData> tlData_;
-
-  // Refresh management fields
-
-  /**
-   * scheduler_ represents the underlying abstraction that will create
-   * the thread that updates stats asynchronously.
-   *
-   * The mutex specifically synchronizes access to scheduler_.  It is
-   * leveraged to ensure all public APIs are thread-safe.
-   *
-   * The refreshPeriodMs_ is the amount of time that must elapse before
-   * the underlying cached data is updated.  It is atomic as it is read
-   * under the dataMutex_ but can also be read/set under schedulerMutex_.
-   */
-  std::unique_ptr<folly::FunctionScheduler> scheduler_;
-  std::mutex schedulerMutex_;
-  std::atomic<std::chrono::milliseconds> refreshPeriodMs_{
-      std::chrono::milliseconds(0)};
-
- private:
-  // Wrapper for updating and retiring old cached data_ via RCU.
-  void modifyData(ResourceData* newData);
 };
 
 } // namespace proxygen
