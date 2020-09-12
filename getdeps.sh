311f90015b4f412178783902c43014c34f92321e
Luca Niccolini <lniccolini@fb.com>
2020-09-12 14:57:02 -0700

    run some more linters
    Reviewed By: mjoras
    
    Differential Revision: D23666633
    
    fbshipit-source-id: f72277a6548af112574ac78a534df3baa8bb21de

--- getdeps.sh
+++ getdeps.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 set -xeo pipefail
 
 TOOLCHAIN_DIR=/opt/rh/devtoolset-8/root/usr/bin
