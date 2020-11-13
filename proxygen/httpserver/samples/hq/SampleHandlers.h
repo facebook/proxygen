6aa0fcbd1dc035e5f1deeebad6ad9b7684c7605f
Jason Huh <jaeseok.huh.0@gmail.com>
2020-11-14 08:36:57 +0900

    Correct the max allowed length

--- proxygen/httpserver/samples/hq/SampleHandlers.h
+++ proxygen/httpserver/samples/hq/SampleHandlers.h
@@ -673,8 +673,8 @@ class RandBytesGenHandler : public BaseSampleHandler {
     error_ = true;
   }
 
-  const uint64_t kMaxAllowedLength{1000 * 1024 * 1024}; // 1 GB
-  const uint64_t kMaxChunkSize{100 * 1024};             // 100 KB
+  const uint64_t kMaxAllowedLength{1ULL * 1024 * 1024 * 1024}; // 1 GB
+  const uint64_t kMaxChunkSize{100ULL * 1024};             // 100 KB
   const std::string kErrorMsg = folly::to<std::string>(
       "More than 1GB of data requested. ", "Please request for smaller size.");
   uint64_t respBodyLen_;
