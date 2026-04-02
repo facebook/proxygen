89a372d53a90f2d30b0f0dff6cb0cf35936c584b
Taemin Park <ptm2316@meta.com>
2026-04-01 23:32:57 -0700

    Refactor exceptions in the certificate verifier
    Summary:
    - This diff is a part of efforts to eliminate exceptions in the fizz client — certificate verifier in Protocol.
    - Remove exceptions and use return-based error model instead, which makes excpetion thowing functions return Status, with additional parameters — param-out ret, and err.
    - Update the call sites of the refactored functions to populate or handle the returned status.
    
    Reviewed By: zalecodez
    
    Differential Revision: D92906166
    
    fbshipit-source-id: 15ab366aeba0048c92f536eef89828035fe66da8

--- proxygen/lib/http/coro/test/TestUtils.h
+++ proxygen/lib/http/coro/test/TestUtils.h
@@ -19,10 +19,12 @@ class InsecureVerifierDangerousDoNotUseInProduction
  public:
   ~InsecureVerifierDangerousDoNotUseInProduction() override = default;
 
-  [[nodiscard]] std::shared_ptr<const folly::AsyncTransportCertificate> verify(
-      const std::vector<std::shared_ptr<const fizz::PeerCert>>& certs)
-      const override {
-    return certs.front();
+  fizz::Status verify(std::shared_ptr<const fizz::Cert>& ret,
+                      fizz::Error& /* err */,
+                      const std::vector<std::shared_ptr<const fizz::PeerCert>>&
+                          certs) const override {
+    ret = certs.front();
+    return fizz::Status::Success;
   }
 
   [[nodiscard]] std::vector<fizz::Extension> getCertificateRequestExtensions()
